8a6dcf8405a4fdd146f23cc5ad8e658137f1f76f
jinyong.park <jiny3360@nine2021.com>
2023-01-26 17:50:56 +0900

    feature : init
    - framer motion 라이브러리 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import React from 'react';
+import { motion } from "framer-motion"
+import AboutBox from "@/components/aboutBox";
 
 type Props = {};
 
 export default function Page({}: Props) {
-  return <div>Page</div>;
+  return <div>
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-center my-10 text-5xl text-gray-400 font-mono">ABOUT</h1>
+      <motion.div >
+        <AboutBox/>
+      </motion.div>
+    </div>
+  </div>;
 }
